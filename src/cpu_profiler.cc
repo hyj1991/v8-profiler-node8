a23dbcd0d5fd319ca3f5e203a681f7fe8484e6d7
Nick Johnstone <ncwjohnstone@gmail.com>
2020-06-17 15:51:29 +1200

    Use old style of CollectSample for Node 8 and 9

--- src/cpu_profiler.cc
+++ src/cpu_profiler.cc
@@ -78,6 +78,10 @@ namespace nodex {
   }
 
   NAN_METHOD(CpuProfiler::CollectSample) {
+#if (NODE_MODULE_VERSION > 0x003B)
     v8::CpuProfiler::CollectSample(v8::Isolate::GetCurrent());
+#else
+    v8::Isolate::GetCurrent()->GetCpuProfiler()->CollectSample();
+#endif
   }
 } //namespace nodex
