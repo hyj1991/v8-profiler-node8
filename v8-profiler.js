c8ce86a52c6b193310a288fe49d1f68a1de7101f
hyj1991 <yeekwanvong@gmail.com>
2022-06-04 14:28:00 +0800

    feat: compatible with 7.3.0@next

--- v8-profiler.js
+++ v8-profiler.js
@@ -1,1 +1,14 @@
-module.exports = require('v8-profiler-next');
+'use strict';
+
+const profiler = require('v8-profiler-next');
+
+if (process.env.V8_PROFILER_EAGER_LOGGING) {
+  const startProfiling = profiler.startProfiling;
+  profiler.startProfiling = function (name, recsamples) {
+    startProfiling.call(profiler, name, recsamples, 1);
+  }
+}
+
+profiler.setProfilesCleanupLimit(1000);
+
+module.exports = profiler;
