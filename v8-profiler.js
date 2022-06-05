a6ad2997a9a8639d260cfa6de1178bb4db41df5e
hyj1991 <yeekwanvong@gmail.com>
2022-06-05 08:10:40 +0800

    fixup

--- v8-profiler.js
+++ v8-profiler.js
@@ -2,11 +2,12 @@
 
 const profiler = require('v8-profiler-next');
 
-if (process.env.V8_PROFILER_EAGER_LOGGING) {
-  const startProfiling = profiler.startProfiling;
-  profiler.startProfiling = function (name, recsamples) {
-    startProfiling.call(profiler, name, recsamples, 1);
+const startProfiling = profiler.startProfiling;
+profiler.startProfiling = function (name, recsamples, mode) {
+  if ([0, 1].indexOf(mode) === -1) {
+    mode = process.env.V8_PROFILER_EAGER_LOGGING ? 1 : 0;
   }
+  startProfiling.call(profiler, name, recsamples, mode);
 }
 
 profiler.setProfilesCleanupLimit(1000);
